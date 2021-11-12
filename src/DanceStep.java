6d5d4bac8fb9ed821285afbc5e5d35ecc77b3667
Phil Kirlin <pkirlin@users.noreply.github.com>
2021-11-12 16:58:57 -0600

    Add files via upload

---
+++ src/DanceStep.java
@@ -0,0 +1,7 @@
+/**
+ * A DanceStep is a enumerated type (like a class, but simpler) that
+ * represents a single step of a dance.  There are only seven possible steps.
+ */
+public enum DanceStep {
+    FORWARD, BACKWARD, STEP_RIGHT, STEP_LEFT, TURN_RIGHT, TURN_LEFT, PAUSE
+}
